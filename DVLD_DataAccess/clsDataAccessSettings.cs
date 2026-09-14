dc11542cf4922208948b6ab5a7b30dc8fbfccfad
Mohamed almaho <mmoho92@gmail.com>
2026-09-14 10:54:28 +0300

    Update database connection string

--- DVLD_DataAccess/clsDataAccessSettings.cs
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -4,6 +4,6 @@ namespace DVLD_DataAccess
 {
     static class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=123456;";
+        public static string ConnectionString = "Server=.;Database=DVLD;Integrated Security=True;";
     }
 }
